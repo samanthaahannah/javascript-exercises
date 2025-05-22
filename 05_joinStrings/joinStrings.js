12592441210ddbf198717622e1f08878b8854fb0
samanthaahannah <samanthaahannah@outlook.com>
2025-05-22 15:42:48 +0100

    Complete JS exercise 5 step 2

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,7 +9,12 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years ols.";
 
 
 
