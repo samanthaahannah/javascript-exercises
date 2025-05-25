688577bf7f7da3e662ba12373c0f542b326cb5ce
samanthaahannah <samanthaahannah@outlook.com>
2025-05-25 17:36:30 +0100

    Complete JS exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const array = [];
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+    if (a < 0 || b < 0) return "ERROR"
+    if (a > b){
+        const tempNumber = a;
+        a = b;
+        b = tempNumber;
+    }
 
+    let total = 0;
+    for (let i = a; i <= b; i++){
+        total+=i;
+    }
+    return total;
 };
 
 // Do not edit below this line
