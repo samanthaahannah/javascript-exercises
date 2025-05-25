b7e8e4f4c1345434f4ae7ce134ea70de2b197c60
samanthaahannah <samanthaahannah@outlook.com>
2025-05-25 14:39:06 +0100

    Complete JS exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = "";
+    if (num < 0)return "ERROR"
+    for(let i = 0; i < num; i++){
+        string+=word;
+    }
+    return string;
 };
 
 // Do not edit below this line
