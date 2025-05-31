39ee264b134e084f73e070336fe5c0efeb2ecf73
samanthaahannah <samanthaahannah@outlook.com>
2025-05-31 17:05:00 +0100

    Complete JS exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    let stringToNo = 0;
+    stringToNo = Number(n);
+    if (stringToNo < 0){
+        return "OOPS";
+    }else if (stringToNo >= 0 && stringToNo < 1){
+        return 0;
+    }else if (stringToNo <= 2){
+        return 1;
+    
+    }else{
+        return fibonacci(stringToNo-1) + fibonacci(stringToNo-2);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
