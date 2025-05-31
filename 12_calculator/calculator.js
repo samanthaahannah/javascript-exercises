58b19357d0fd5f7ea342553d1b13d359289f84be
samanthaahannah <samanthaahannah@outlook.com>
2025-05-31 15:20:14 +0100

    Complete JS Exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initial = 0;
+	return arr.reduce((a, b) => a + b, initial,);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+	return arr.reduce((a, b) => a * b,);
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let factoral = 1;
+	for (let i = 1; i <= n; i++){
+    factoral *= i;
+  }
+  return factoral;
 };
 
 // Do not edit below this line
