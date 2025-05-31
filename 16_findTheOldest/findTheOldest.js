08aca9112f4d842bf2348b92ebab3bade0c1bb7f
samanthaahannah <samanthaahannah@outlook.com>
2025-05-31 19:23:53 +0100

    Complete JS  exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 
+const findTheOldest = function (people){
+  return people.reduce((a, b) => {
+      const last = getAge(a.yearOfBirth, a.yearOfDeath);
+      const next = getAge(b.yearOfBirth, b.yearOfDeath);
+      if (last < next){
+        return b;
+      } else{
+        return a;
+      }
+    });
 };
 
 // Do not edit below this line
