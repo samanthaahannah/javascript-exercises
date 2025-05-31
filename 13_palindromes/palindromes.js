840263728895c5d53fd2fe5a430cf09c314252d3
samanthaahannah <samanthaahannah@outlook.com>
2025-05-31 16:39:20 +0100

    Complete JS exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let isPalindome = false;
+    const lettersNums = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const cleaned = string
+   .toLowerCase()
+   .split('')
+   .filter((char) => lettersNums.includes(char))
+   .join('');
+
+   let reversed = cleaned.split('').reverse().join('');
+
+   if (cleaned === reversed){
+    return isPalindome = true;
+   }else{
+    return isPalindome = false;
+   }
+
 
 };
 
